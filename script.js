91178d7d9983c3613decf2c4d3a80aa9bdb361f7
Viktor Luthman <Viktorll95@hotmail.com>
2024-09-04 20:36:38 +0200

    Started process of adding pieces

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const board = document.querySelector("#board");
 
+// 42 cells | 1 - red, 2 - yellow
+const pieces = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+
 for (let i = 0; i < 42; i++) {
   let cell = document.createElement("div");
   cell.className = "cell";
@@ -8,6 +14,14 @@ for (let i = 0; i < 42; i++) {
   cell.onmouseenter = () => {
     onMouseEnteredColumn(i % 7);
   };
+
+  cell.onClick = () => {
+    onColumnClick(i % 7);
+  };
+}
+
+function onColumnClick(column) {
+  pieces.filter((_, index) => index % 7 === column);
 }
 
 function onMouseEnteredColumn(column) {
